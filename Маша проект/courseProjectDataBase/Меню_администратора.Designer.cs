d126dc278f907e39dc549b5a24355de95d9da7f9
MariaChukanova <91369732+MariaChukanova@users.noreply.github.com>
2022-06-11 12:29:42 +0300

    add some changes

--- Маша проект/courseProjectDataBase/Меню_администратора.Designer.cs
+++ Маша проект/courseProjectDataBase/Меню_администратора.Designer.cs
@@ -188,6 +188,7 @@
             this.label2.Size = new System.Drawing.Size(90, 25);
             this.label2.TabIndex = 16;
             this.label2.Text = "Фамилия";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // label3
             // 
